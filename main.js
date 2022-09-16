9504393b12f28c8ebb63bae6f67d55362a66a728
John Watkins <johnwes87@protonmail.com>
2022-09-16 14:44:03 -0500

    1st Commit

---
+++ main.js
@@ -0,0 +1,41 @@
+
+function getComputerChoice() {
+    const hand = ['rock', 'paper', 'scissors']
+    choice = Math.floor(Math.random() * hand.length);
+    return hand[choice];
+
+}
+
+
+let playerScore = 0;
+let cpuScore = 0;
+
+function play(playerSelection, cpuSelection) {
+    if (playerSelection === cpuSelection ) {
+        return "draw"
+    } 
+    if (
+        (playerSelection === 'rock' && cpuSelection === 'scissors') ||
+        (playerSelection === 'paper' && cpuSelection === 'rock') ||
+        (playerSelection === 'scissors' && cpuSelection === 'paper')
+        )
+     {
+        playerScore++;
+        return "Player wins"
+
+    }
+    if (
+        (cpuSelection === 'rock' && playerSelection === 'scissors') ||
+        (cpuSelection === 'paper' && playerSelection === 'rock') ||
+        (cpuSelection === 'scissors' && playerSelection === 'paper')
+    ) {
+        cpuScore++;
+        return "CPU wins";
+
+    }
+}
+
+const cpuSelection = getComputerChoice();
+const playerSelection = prompt("Rock, Paper, Scissors");
+
+console.log(play(playerSelection, cpuSelection))
